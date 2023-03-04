f809869c2b2301335baf21dadb130c0c2a62ebb6
Magda Slifierz <m.slifierz@gmail.com>
2023-03-03 23:48:55 -0800

    add

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('members/', include('django.contrib.auth.urls')), #this is needed for authentication it taks care from django package
+    path('members/',include('members.urls')), 
 ]
